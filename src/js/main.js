b6e30388f4312a1649c85d1c9a5bf04c36d6f171
Daniel Schreurs <daniel.schreurs@hotmail.com>
2022-02-19 20:40:14 +0100

    :tada: Initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,59 +1,54 @@
+const endpoint = "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json"
+const cities = [];
+const searchForm = document.querySelector(".search-form");
+const suggestions = document.querySelector(".suggestions");
+
 if (window.fetch) {
-    const App = {
-        init() {
-            this.searchInput = document.getElementById('search');
-            this.searchForm = document.querySelector('.search-form');
-            this.suggestions = document.querySelector('.suggestions');
-            this.endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
-            this.cities = [];
-            fetch(this.endpoint).then(response => {
-                if (response.status === 200) {
-                    response.json().then(jsonResponse => {
-                            this.cities.push(...jsonResponse);
-                        }
-                    );
-                } else {
-                    console.error("Nous ne parvenons pas à récupérer la ressource...");
-                }
-            }).catch(error => {
-                console.error("Un problème de réseau est survenu. Vérifier votre connexion internet.");
-                console.error(error);
-            });
-            this.searchInput.addEventListener("keyup", (event) => {
-                console.log(event.currentTarget.value);
-            });
-            this.searchForm.addEventListener("submit", event => {
-                event.preventDefault();
-            })
-            this.searchInput.addEventListener("keyup", (event) => {
-                this.displayMatches(event.currentTarget.value);
-            });
-
-        },
-        findMatches(wordToMatch) {
-            return this.cities.filter(item => {
-                const regex = new RegExp(wordToMatch, 'gi');
-                return item.city.match(regex) || item.state.match(regex);
-            });
-        },
-        numberWithSpace(x) {
-            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-        },
-
-        displayMatches(wordToMatch) {
-            this.suggestions.innerHTML = this.findMatches(wordToMatch).map(place => {
-                const regex = new RegExp(wordToMatch, 'gi');
-                const cName = place.city.replace(regex, `<span class="hl">${wordToMatch}</span>`);
-                const sName = place.state.replace(regex, `<span class="hl">${wordToMatch}</span>`);
-                return `<li>
-        <span class="name">${cName}, ${sName}</span>
-        <span class="population">${this.numberWithSpace(place.population)}</span>
-        </li>`;
-            }).join('');
-        },
-
-
-    };
-    App.init();
+    // exécuter ma requête fetch ici
+    fetch(endpoint)
+        .then(response => {
+            if (response.status === 200) {
+                response.json().then(responseJSON => {
+                        cities.push(...responseJSON);
+                        console.log(cities);
+                    }
+                )
+            } else {
+                console.error(`Oups ! ${response.status}`);
+            }
+        }).catch(error => {
+        console.log(`Aie !! ${error}`);
+    });
+} else {
+    // Faire quelque chose avec XMLHttpRequest?
 }
+
+function findMatches(wordToMatch) {
+    return cities.filter(place => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.city.match(regex) || place.state.match(regex);
+    });
+}
+
+function numberWithSpace(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+}
+
+function displayMatches(wordToMatch) {
+    suggestions.innerHTML = findMatches(wordToMatch).map(item => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return `<li>
+        <span class="name">${item.city.replace(regex, `<span class="hl">${wordToMatch}</span>`)},${item.state.replace(regex, `<span class="hl">${wordToMatch}</span>`)}</span>
+        <span class="population">${numberWithSpace(item.population)}</span></li>`;
+    }).join('');
+
+}
+
+searchForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+})
+
+searchForm.querySelector("#search").addEventListener("keyup", (event) => {
+    displayMatches(event.currentTarget.value);
+})
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -4,4 +4,5 @@ let mix = require('laravel-mix');
 
 mix.js('src/js/main.js', 'js').sourceMaps()
     .sass('src/scss/main.scss', 'css').sourceMaps()
-    .setPublicPath('dist').browserSync("http://localhost:8000");
+    .setPublicPath('dist')
+    .browserSync("http://localhost:8000");
