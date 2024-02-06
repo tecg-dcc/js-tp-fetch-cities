f4373ff388034aefe525df118fd160de1ab3be51
Daniel Schreurs <daniel.schreurs@hotmail.com>
2024-02-06 19:24:41 +0100

    :tada: Initial commit

--- webpack.mix.js
+++
@@ -1,8 +0,0 @@
-// webpack.mix.js
-
-let mix = require('laravel-mix');
-
-mix.js('src/js/main.js', 'js').sourceMaps()
-    .sass('src/scss/main.scss', 'css').sourceMaps()
-    .setPublicPath('dist')
-    .browserSync("http://localhost:8000");
