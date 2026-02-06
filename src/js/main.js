2f69f30cd29ac6c55d7952639cdd416be0606f48
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2026-02-06 15:24:57 +0100

    Refactor HTML structure and update JavaScript fetch logic for city data

--- src/js/main.js
+++ src/js/main.js
@@ -1,54 +1,30 @@
-const endpoint = "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json"
-const cities = [];
-const searchForm = document.querySelector(".search-form");
-const suggestions = document.querySelector(".suggestions");
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-if (window.fetch) {
-    // exécuter ma requête fetch ici
-    fetch(endpoint)
-        .then(response => {
-            if (response.status === 200) {
-                response.json().then(responseJSON => {
-                        cities.push(...responseJSON);
-                        console.log(cities);
-                    }
-                )
-            } else {
-                console.error(`Oups ! ${response.status}`);
+try {
+    const request = await fetch(endpoint);
+    if (request.status === 200) {
+        const data = await request.json();
+        for (const city of data) {
+            document.body.insertAdjacentHTML('beforeend', `<div>${city.city} - ${city.population}</div>`);
+        }
+    }
+} catch (e) {
+    console.log(e.message);
+}
+
+fetch(endpoint).then((request) => {
+    if (request.status === 200) {
+        request.json().then((data) => {
+            for (const city of data) {
+                document.body.insertAdjacentHTML('beforeend', `<div>${city.city} - ${city.population}</div>`);
             }
-        }).catch(error => {
-        console.log(`Aie !! ${error}`);
-    });
-} else {
-    // Faire quelque chose avec XMLHttpRequest?
-}
+        });
+    }
+}).catch((e) => {
+    console.log(e.message);
+});
 
-function findMatches(wordToMatch) {
-    return cities.filter(place => {
-        const regex = new RegExp(wordToMatch, 'gi');
-        return place.city.match(regex) || place.state.match(regex);
-    });
-}
 
-function numberWithSpace(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
-}
 
-function displayMatches(wordToMatch) {
-    suggestions.innerHTML = findMatches(wordToMatch).map(item => {
-        const regex = new RegExp(wordToMatch, 'gi');
-        return `<li>
-        <span class="name">${item.city.replace(regex, `<span class="hl">${wordToMatch}</span>`)},${item.state.replace(regex, `<span class="hl">${wordToMatch}</span>`)}</span>
-        <span class="population">${numberWithSpace(item.population)}</span></li>`;
-    }).join('');
 
-}
-
-searchForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-})
-
-searchForm.querySelector("#search").addEventListener("keyup", (event) => {
-    displayMatches(event.currentTarget.value);
-})
 
