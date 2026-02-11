f0b01e314506a328ee624c293dbc5c353792fb33
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2026-02-11 15:29:12 +0100

    Initial commit

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,3 +1,55 @@
 import {settings} from "./settings";
 
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const app = {
+    init() {
+        this.cities = null;
+
+        this.fetchCities();
+        this.citiesContainerElement = document.querySelector(settings.citiesContainerSelector);
+        this.searchInputElement = document.getElementById(settings.searchInputID);
+
+        this.searchInputElement.addEventListener('input', (evt) => {
+            if (this.cities) {
+                const userText = evt.currentTarget.value.toLowerCase();
+
+                this.generateCitiesItems(this.cities.filter((city) => {
+                    return city.city.toLowerCase().includes(userText);
+                }));
+            }
+
+        });
+
+    },
+    fetchCities() {
+        fetch(endpoint).then((response) => {
+            if (response.ok && response.status === 200) {
+                response.json().then((data) => {
+                    this.cities = data;
+                });
+            } else {
+                // Aie ici ce n’est pas bon...
+                console.log(`response.status... ${response.status}`)
+            }
+
+        }).catch((reason) => {
+            console.log(reason)
+        });
+    },
+    generateCitiesItems(cities) {
+        this.citiesContainerElement.innerHTML = '';
+
+        for (const city of cities) {
+            this.citiesContainerElement.insertAdjacentHTML('beforeend', `<li>
+           <span class="name">${city.city}</span>
+           <span class="population">${city.population}</span>
+   </li>`)
+        }
+    },
+
+}
+
+
+app.init();
+
--- src/ts/settings.ts
+++ src/ts/settings.ts
@@ -1,9 +1,4 @@
 export const settings = {
-    suggestionListSelector: '.suggestions',
-    inputElementID: 'search',
-    liHTML(city) {
-        return `<li>
-        <span class="name">${city.city} - ${city.state}</span>
-        <span class="population">${city.population}</span></li>`;
-    }
+    citiesContainerSelector: '.suggestions',
+    searchInputID: "search"
 }
